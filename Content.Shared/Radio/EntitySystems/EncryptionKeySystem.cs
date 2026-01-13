bec2d63bbb5413d02d2a57927f831e5f107d8faa
Livyathan <notlivyathan@gmail.com>
2026-01-13 14:57:23 -0500

    synth tweaks and some new markings

--- Content.Shared/Radio/EntitySystems/EncryptionKeySystem.cs
+++ Content.Shared/Radio/EntitySystems/EncryptionKeySystem.cs
@@ -168,8 +168,7 @@ public sealed partial class EncryptionKeySystem : EntitySystem
     {
         if (!args.IsInDetailsRange
             || !component.ExamineWhileLocked && !component.KeysUnlocked // Goobstation
-            || !component.ExamineWhileLocked && TryComp<WiresPanelComponent>(uid, out var panel) && !panel.Open // Goobstation
-            || !component.ShowOnExamine) // HardLight
+            || !component.ExamineWhileLocked && TryComp<WiresPanelComponent>(uid, out var panel) && !panel.Open) // Goobstation
             return;
 
         if (component.KeyContainer.ContainedEntities.Count == 0)
