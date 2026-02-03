eb9cf192b35c9bfb10df7dbc39ec2b199afc9833
fenndragon <fenndragon@gmail.com>
2026-02-03 12:40:51 -0700

    Add check for ThievingComponent in strip logic

--- Content.Shared/Strip/SharedStrippableSystem.cs
+++ Content.Shared/Strip/SharedStrippableSystem.cs
@@ -692,6 +692,9 @@ public abstract class SharedStrippableSystem : EntitySystem
 
         if (HasComp<BypassInteractionChecksComponent>(viewer.Value))
             return false;
+            
+        if (TryComp<ThievingComponent>(viewer.Value, out var thief) && thief.IdentifyHidden) // Mono
+            return false
 
         if (HasComp<IdentifyHiddenStripComponent>(viewer.Value)) // HardLight
             return false;
