ef06fef929bf080a421b95dc8b8f6d692bae5503
Livyathan <notlivyathan@gmail.com>
2026-02-05 06:46:54 -0500

    Port Starlight Thermal & Night Visions

--- Content.Shared/Eye/Blinding/Systems/EyeProtectionSystem.cs
+++ Content.Shared/Eye/Blinding/Systems/EyeProtectionSystem.cs
@@ -19,7 +19,19 @@ namespace Content.Shared.Eye.Blinding.Systems
 
             SubscribeLocalEvent<EyeProtectionComponent, GetEyeProtectionEvent>(OnGetProtection);
             SubscribeLocalEvent<EyeProtectionComponent, InventoryRelayedEvent<GetEyeProtectionEvent>>(OnGetRelayedProtection);
+
+            //🌟Starlight🌟 start
+            SubscribeLocalEvent<NightVisionComponent, GetEyeProtectionEvent>(OnGetProtection);
+            SubscribeLocalEvent<ThermalVisionComponent, GetEyeProtectionEvent>(OnGetProtection);
+            //🌟Starlight🌟 end
         }
+
+        //🌟Starlight🌟 start
+        private void OnGetProtection(Entity<NightVisionComponent> ent, ref GetEyeProtectionEvent args)
+            => args.Protection -= TimeSpan.FromSeconds(9);  //We'll need to put this constant somewhere in the prototypes...later...
+        private void OnGetProtection(Entity<ThermalVisionComponent> ent, ref GetEyeProtectionEvent args)
+            => args.Protection -= TimeSpan.FromSeconds(12);
+        //🌟Starlight🌟 end
 
         private void OnGetRelayedProtection(EntityUid uid, EyeProtectionComponent component,
             InventoryRelayedEvent<GetEyeProtectionEvent> args)
@@ -43,7 +55,7 @@ namespace Content.Shared.Eye.Blinding.Systems
             var ev = new GetEyeProtectionEvent();
             RaiseLocalEvent(args.User, ev);
 
-            var time = (float) (component.StatusEffectTime - ev.Protection).TotalSeconds;
+            var time = (float)(component.StatusEffectTime - ev.Protection).TotalSeconds;
             if (time <= 0)
                 return;
 
