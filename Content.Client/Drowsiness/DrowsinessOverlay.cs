45ddeb1d5c0bfa8afe39b63f303c7dcf4e28a9f6
Livyathan <notlivyathan@gmail.com>
2026-04-05 19:28:36 -0400

    Port WizDen Refactor SeeingRainbows to New Status Effect System #38620

--- Content.Client/Drowsiness/DrowsinessOverlay.cs
+++ Content.Client/Drowsiness/DrowsinessOverlay.cs
@@ -1,7 +1,5 @@
-using Content.Shared.Bed.Sleep;
 using Content.Shared.Drowsiness;
 using Content.Shared.StatusEffectNew;
-using Content.Shared.StatusEffectNew.Components;
 using Robust.Client.Graphics;
 using Robust.Client.Player;
 using Robust.Shared.Enums;
@@ -17,13 +15,11 @@ public sealed class DrowsinessOverlay : Overlay
     [Dependency] private readonly IPlayerManager _playerManager = default!;
     [Dependency] private readonly IEntitySystemManager _sysMan = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
-    private readonly SharedStatusEffectsSystem _statusEffects = default!;
+    private readonly StatusEffectsSystem _statusEffects = default!;
 
     public override OverlaySpace Space => OverlaySpace.WorldSpace;
     public override bool RequestScreenTexture => true;
     private readonly ShaderInstance _drowsinessShader;
-
-    private EntityQuery<StatusEffectComponent> _statusQuery;
 
     public float CurrentPower = 0.0f;
 
@@ -34,9 +30,9 @@ public sealed class DrowsinessOverlay : Overlay
     public DrowsinessOverlay()
     {
         IoCManager.InjectDependencies(this);
-        _statusEffects = _sysMan.GetEntitySystem<SharedStatusEffectsSystem>();
 
-        _statusQuery = _entityManager.GetEntityQuery<StatusEffectComponent>();
+        _statusEffects = _sysMan.GetEntitySystem<StatusEffectsSystem>();
+
         _drowsinessShader = _prototypeManager.Index(DrowsinessShaderId).InstanceUnique();
     }
 
@@ -47,22 +43,11 @@ public sealed class DrowsinessOverlay : Overlay
         if (playerEntity == null)
             return;
 
-        if (!_statusEffects.TryEffectsWithComp<DrowsinessStatusEffectComponent>(playerEntity, out var drowsinessEffects))
+        if (!_statusEffects.TryGetEffectsEndTimeWithComp<DrowsinessStatusEffectComponent>(playerEntity, out var endTime))
             return;
 
-        TimeSpan? remainingTime = TimeSpan.Zero;
-        foreach (var (_, _, statusEffectComp) in drowsinessEffects)
-        {
-            if (statusEffectComp.EndEffectTime > remainingTime)
-                remainingTime = statusEffectComp.EndEffectTime;
-        }
-
-        if (remainingTime is null)
-            return;
-
-        var curTime = _timing.CurTime;
-        var timeLeft = (float)(remainingTime - curTime).Value.TotalSeconds;
-
+        endTime ??= TimeSpan.MaxValue;
+        var timeLeft = (float)(endTime - _timing.CurTime).Value.TotalSeconds;
         CurrentPower += 8f * (0.5f * timeLeft - CurrentPower) * args.DeltaSeconds / (timeLeft + 1);
     }
 
