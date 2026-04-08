80c1f471dbc0aa234fa2381d9bb83efdd7601434
Livyathan <notlivyathan@gmail.com>
2026-04-07 21:02:04 -0400

    Port WizDen Traits Status Effect Support & PainNumbness Status Effect PR #41646

--- Content.Shared/StatusEffectNew/StatusEffectSystem.Relay.cs
+++ Content.Shared/StatusEffectNew/StatusEffectSystem.Relay.cs
@@ -1,3 +1,5 @@
+using Content.Shared.Damage.Events;
+using Content.Shared.Mobs.Events;
 using Content.Shared.Movement.Events;
 using Content.Shared.Movement.Systems;
 using Content.Shared.Rejuvenate;
@@ -18,6 +20,9 @@ public sealed partial class StatusEffectsSystem
 
         SubscribeLocalEvent<StatusEffectContainerComponent, RefreshMovementSpeedModifiersEvent>(RelayStatusEffectEvent);
         SubscribeLocalEvent<StatusEffectContainerComponent, UpdateCanMoveEvent>(RelayStatusEffectEvent);
+
+        SubscribeLocalEvent<StatusEffectContainerComponent, BeforeForceSayEvent>(RelayStatusEffectEvent);
+        SubscribeLocalEvent<StatusEffectContainerComponent, BeforeAlertSeverityCheckEvent>(RelayStatusEffectEvent);
 
         SubscribeLocalEvent<StatusEffectContainerComponent, AccentGetEvent>(RelayStatusEffectEvent);
     }
