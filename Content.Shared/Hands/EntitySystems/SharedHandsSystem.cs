c15e186c3c811fee9d2d3258003e14e474d65797
Livyathan <notlivyathan@gmail.com>
2026-04-11 12:54:08 -0400

    Port WizDen Simplify Hands UI Code #42534

--- Content.Shared/Hands/EntitySystems/SharedHandsSystem.cs
+++ Content.Shared/Hands/EntitySystems/SharedHandsSystem.cs
@@ -92,6 +92,8 @@ public abstract partial class SharedHandsSystem
 
         ent.Comp.Hands.Add(handName, hand);
         AddToSortedHands(ent.Comp, handName, hand.Location); // Shitmed, // HardLight: handsComp<ent.Comp & handLocation<hand.Location
+        // we use LINQ + ToList instead of the list sort because it's a stable sort vs the list sort
+        ent.Comp.SortedHands = ent.Comp.SortedHands.OrderBy(handId => ent.Comp.Hands[handId].Location).ToList();
         Dirty(ent);
 
         OnPlayerAddHand?.Invoke((ent, ent.Comp), handName, hand.Location);
